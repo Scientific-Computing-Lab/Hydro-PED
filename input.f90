afa32f98885cefd271e763568c054dffaf4d1974
Harel Levin <harellevin@gmail.com>
2019-07-28 11:55:15 +0300

    Correction of copyrights

--- input.f90
+++ input.f90
@@ -14,9 +14,10 @@
 !    * Redistributions in binary form must reproduce the above copyright
 !      notice, this list of conditions and the following disclaimer in the
 !      documentation and/or other materials provided with the distribution.
-!    * Neither the name of Harel Levin or Gal Oren, nor the
-!      names of its contributors may be used to endorse or promote products
-!      derived from this software without specific prior written permission.
+!    * Neither the name of Eyal Shalev, Vladimir Lyakhovsky, Harel Levin or 
+!      Gal Oren, nor the names of its contributors may be used to endorse 
+!      or promote products derived from this software without specific prior 
+!      written permission.
 !
 ! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 ! AND
